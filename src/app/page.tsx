b23a8572004212c5fcb80d2fdb851e21725cfc22
Pupil1999 <bfangong@gmail.com>
2024-07-19 01:59:38 +0800

    deleted:    src/app/favicon.ico 	modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 
     const client = createPublicClient({
       chain: mainnet,
-      transport: http(`https://mainnet.infura.io/v3/381bfebfafac44e48a185f69401bc416`),
+      transport: http(`https://mainnet.infura.io/v3/your_api_key`),
     });
 
     const getBlockInfo = async () => {
